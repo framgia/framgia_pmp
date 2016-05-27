5d7b377c289182c5563ff8448c1b19f1db5856af
ngocvt <ngocvu.uet@gmail.com>
2016-05-27 16:07:01 +0700

    add more row

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,19 @@ module ApplicationHelper
       flash[:success] = t "sessions.not_admin"
     end
   end
+
+  def link_to_remove_fields label, f
+    field = f.hidden_field :_destroy
+    link = link_to label, "#", onclick: "remove_fields(this)", remote: true
+    field + link
+  end
+
+  def link_to_add_fields label, f, assoc
+    new_obj = f.object.class.reflect_on_association(assoc).klass.new
+    fields = f.fields_for assoc, new_obj, child_index: "new_#{assoc}" do |builder|
+      render "shared/#{assoc.to_s.singularize}_fields", f: builder
+    end
+    link_to label, "#", onclick: "add_fields(this, \"#{assoc}\",
+      \"#{escape_javascript(fields)}\")", remote: true
+  end
 end
