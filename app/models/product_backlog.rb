661a1b2382c438247e55679980261ce03a0574b4
Hang Xuka <phamhangmta94@gmail.com>
2016-07-14 14:15:50 +0700

    calculate remaining time

--- app/models/product_backlog.rb
+++ app/models/product_backlog.rb
@@ -6,4 +6,16 @@ class ProductBacklog < ActiveRecord::Base
 
   delegate :name, to: :project, prefix: true
   delegate :name, :id, to: :sprint, prefix: true, allow_nil: true
+
+  def calculate_remaining_time
+     tmp_remaining = 0
+     if sprint.present?
+       sprint.activities.where("product_backlog_id = ?", id).each do |activity|
+         tmp_remaining += activity.log_works.last.remaining_time
+       end
+       self.remaining = tmp_remaining
+       save!
+     end
+     self.remaining
+  end
 end
