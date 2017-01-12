792819f9915a59c887ecb951659271cd12d17ad9
luuthomvp <luuthomvp@gmail.com>
2017-01-12 14:15:45 +0700

    calculate_burned_story

--- app/models/concerns/update_performance.rb
+++ app/models/concerns/update_performance.rb
@@ -2,8 +2,7 @@ module UpdatePerformance
   extend ActiveSupport::Concern
 
   def update_performance_of_spent_time
-    item_spent_time = ItemPerformance.find_by performance_name:
-      ItemPerformance.performance_names[:spent_time]
+    item_spent_time = ItemPerformance.find_by performance_name: :spent_time
     if item_spent_time
       wpd = sprint.work_performances.of_task_in_day(self.task_id,
         self.master_sprint_id, item_spent_time.id)
@@ -15,6 +14,30 @@ module UpdatePerformance
           item_performance_id: item_spent_time.id,
           master_sprint_id: self.master_sprint.id,
           performance_value: spent_time
+      end
+    end
+  end
+
+  def update_performance_of_burn_story
+    item_burn_story = ItemPerformance.find_by performance_name: :burn_story
+    total_burn_story = ProductBacklog.remaining_time_zero.count
+    sprint.tasks.map do |task|
+      update_or_create_wpd(item_burn_story, self, total_burn_story)
+    end
+  end
+
+  def update_or_create_wpd item, log_work, total_burn_story
+    if item
+      wpd = log_work.sprint.work_performances.of_task_in_day(log_work.task_id,
+        log_work.master_sprint_id, item.id)
+
+      if wpd.any?
+        wpd.first.update_attributes performance_value: total_burn_story
+      else
+        log_work.task.work_performances.create sprint_id: log_work.sprint.id,
+          item_performance_id: item.id,
+          master_sprint_id: log_work.master_sprint.id,
+          performance_value: total_burn_story
       end
     end
   end
